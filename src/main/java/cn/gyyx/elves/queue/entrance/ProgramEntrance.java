74a4305b9ee78efac3aa0358c2e5513d3d53c951
eastFu <492510931@qq.com>
2017-07-09 22:23:12 +0800

    update: create a switch for zookeeper

--- src/main/java/cn/gyyx/elves/queue/entrance/ProgramEntrance.java
+++ src/main/java/cn/gyyx/elves/queue/entrance/ProgramEntrance.java
@@ -27,6 +27,7 @@ public class ProgramEntrance {
 		SpringUtil.PROPERTIES_CONFIG_PATH=configPath+File.separator+"conf"+File.separator+"conf.properties";
 		SpringUtil.LOG4J_CONFIG_PATH=configPath+File.separator+"conf"+File.separator+"log4j.properties";
 		SpringUtil.MYBATIS_CONFIG_PATH="file:"+configPath+File.separator+"conf"+File.separator+"mybatis.xml";
+		LOG.info("loadAllConfigFilePath success!");
 	}
 	
 	/**
@@ -35,6 +36,7 @@ public class ProgramEntrance {
 	private static void loadLogConfig() throws Exception{
 		InputStream in=new FileInputStream(SpringUtil.LOG4J_CONFIG_PATH);// 自定义配置
 		PropertyConfigurator.configure(in);
+		LOG.info("loadLogConfig success!");
 	}
 	
 	/**
@@ -42,14 +44,31 @@ public class ProgramEntrance {
 	 */
 	private static void loadApplicationXml() throws Exception{
 		SpringUtil.app = new FileSystemXmlApplicationContext(SpringUtil.SPRING_CONFIG_PATH,SpringUtil.RABBITMQ_CONFIG_PATH);
+		LOG.info("loadApplicationXml success!");
 	}
-	
-	private static void registerZooKeeper(){
-		ZookeeperExcutor zke=new ZookeeperExcutor(PropertyLoader.ZOOKEEPER_HOST,
-				PropertyLoader.ZOOKEEPER_OUT_TIME, PropertyLoader.ZOOKEEPER_OUT_TIME);
-		String nodeName=zke.createNode(PropertyLoader.ZOOKEEPER_ROOT+"/Queue/", "");
-		if(null!=nodeName){
-			zke.addListener(PropertyLoader.ZOOKEEPER_ROOT+"/Queue/", "");
+
+	/**
+	 * 注册模块到zk节点上
+	 */
+	private static void registerZooKeeper() throws Exception{
+		if("true".equalsIgnoreCase(PropertyLoader.ZOOKEEPER_ENABLED)){
+			LOG.info("regist zookeeper ....");
+			ZookeeperExcutor zke=new ZookeeperExcutor(PropertyLoader.ZOOKEEPER_HOST,
+					PropertyLoader.ZOOKEEPER_OUT_TIME, PropertyLoader.ZOOKEEPER_OUT_TIME);
+
+			//创建模块根节点
+			if(null==zke.getClient().checkExists().forPath(PropertyLoader.ZOOKEEPER_ROOT)){
+				zke.getClient().create().creatingParentsIfNeeded().forPath(PropertyLoader.ZOOKEEPER_ROOT);
+			}
+			if(null==zke.getClient().checkExists().forPath(PropertyLoader.ZOOKEEPER_ROOT+"/queue")){
+				zke.getClient().create().creatingParentsIfNeeded().forPath(PropertyLoader.ZOOKEEPER_ROOT+"/queue");
+			}
+
+			String nodeName=zke.createNode(PropertyLoader.ZOOKEEPER_ROOT+"/queue/", "");
+			if(null!=nodeName){
+				zke.addListener(PropertyLoader.ZOOKEEPER_ROOT+"/queue/", "");
+			}
+			LOG.info("register ZooKeeper success!");
 		}
 	}
 	
@@ -57,16 +76,12 @@ public class ProgramEntrance {
 		if(null!=args&&args.length>0){
 			try {
 				loadAllConfigFilePath(args[0]);
-				LOG.info("loadAllConfigFilePath success!");
-				
+
 		    	loadLogConfig();
-				LOG.info("loadLogConfig success!");
 
 				loadApplicationXml();
-				LOG.info("loadApplicationXml success!");
-				
+
 				registerZooKeeper();
-				LOG.info("registerZooKeeper success!");
 			} catch (Exception e) {
 				LOG.error("start queue error:"+ExceptionUtil.getStackTraceAsString(e));
 				System.exit(1);
